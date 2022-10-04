f84c782bbb90047ec1e78f57d51d504dd2c9cb03
Rustam-Asdanov <asdanovrustam@gmail.com>
2022-10-04 16:04:40 +0400

    add color(chalk)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import fs from "fs";
 import bodyParser from "body-parser";
+import validator from "validator";
+import chalk from "chalk";
+
+// chalk
+const successMsg = chalk.bgGreen.bold;
+const warningMsg = chalk.bgYellow;
+const errorMsg = chalk.bgRed;
 
 const app: Application = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,10 +18,12 @@ const add = (a: number, b: number): number => a + b;
 app.get("/:text", (req: Request, res: Response, next: NextFunction) => {
   // const text: string = req.query.text as string; // when you get data from params
   const text: string = req.params.text; // when you get data from url
+  console.log(validator.isEmail(text));
+
   fs.appendFileSync(__dirname + "/notes.txt", text);
   res.send("Start");
 });
 
 app.listen(port, () => {
-  console.log(`Server listen on port ${port}`);
+  console.log(successMsg(`Server listen on port ${port}`));
 });
