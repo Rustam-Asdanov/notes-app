ae0667ab668b09c2df105b7690e1568c40042cdb
Rustam-Asdanov <asdanovrustam@gmail.com>
2022-10-04 17:50:24 +0400

    using yargs for reading commands from command line

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,53 @@ import fs from "fs";
 import bodyParser from "body-parser";
 import validator from "validator";
 import chalk from "chalk";
+import yargs from "yargs";
 
 // chalk
 const successMsg = chalk.bgGreen.bold;
 const warningMsg = chalk.bgYellow;
 const errorMsg = chalk.bgRed;
+const infoMsg = chalk.blue.inverse;
 
 const app: Application = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const port: number = 3000;
 
 const add = (a: number, b: number): number => a + b;
+
+yargs.command({
+  command: "add",
+  describe: "Add a note",
+  handler: function () {
+    console.log("KLyag");
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  handler: function () {
+    console.log("remove");
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: function () {
+    console.log("read");
+  },
+});
+
+yargs.command({
+  command: ["list", "List"],
+  describe: "list all notes",
+  handler: function () {
+    console.log("list");
+  },
+});
+
+console.log(yargs.argv);
 
 app.get("/:text", (req: Request, res: Response, next: NextFunction) => {
   // const text: string = req.query.text as string; // when you get data from params
