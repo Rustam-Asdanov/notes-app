d001c33b3235edcc55f71281414cbfa3ba072890
Rustam-Asdanov <asdanovrustam@gmail.com>
2022-10-04 11:59:59 +0400

    we can get data from url and add to file using filesystem in node

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import fs from "fs";
+import bodyParser from "body-parser";
 
 const app: Application = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 const port: number = 3000;
 
 const add = (a: number, b: number): number => a + b;
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Halo");
+app.get("/:text", (req: Request, res: Response, next: NextFunction) => {
+  // const text: string = req.query.text as string; // when you get data from params
+  const text: string = req.params.text; // when you get data from url
+  fs.appendFileSync(__dirname + "/notes.txt", text);
+  res.send("Start");
 });
 
 app.listen(port, () => {
